8e615fb3a595cce69502748e5659650ef142dd7b
jhavellanedaa <54986746+jhavellanedaa@users.noreply.github.com>
2019-10-28 09:52:25 -0500

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -14,7 +14,7 @@ int main(void)
 {
  std::ofstream fout("1.txt");
  fout.precision(15); fout.setf(std::ios::scientific);
- fout << "# EPS-Delta" << "\t        ";
+ fout << "# EPS-Delta" << "\t ";
  fout << "NBisect" << "\t";
  fout << "NFalsePos" << "\t";
  fout << "NNewton" << "\n";
@@ -24,14 +24,9 @@ int main(void)
   double N = 0;
   fout << Delta << "\t  ";
   fout << bisection(0.1, 2.0, Delta, fun) <<"\t   ";
-  fout << regulafalsi(0.1, 2.0, Delta, fun) << "\t         ";
+  fout << regulafalsi(0.1, 2.0, Delta, fun) << "\t ";
   fout << newton_raphson(0.1, Delta, fun, deriv) << "\n";
-  // std::cout << "Iter Bisection" << N << "\t";
-  //N  = regulafalsi(1, 2, Delta, fun);
-  //std::cout << "Iter RegulaFalsi"  << N << "\t";
-  //N = newton_raphson(1, Delta, fun, deriv);
-  //std::cout << "Iter Newton" << N  << "\n";
-    }
+   }
   fout.close();
   return 0;
 }
@@ -41,6 +36,7 @@ int bisection(double xl, double xu, double Delta, fptr f)
   double xr = xl;
   int N = 0;
   while(1) {
+    N++;
     xr=(xl+xu)/2;
     if (std::fabs(f(xr)) <= Delta) {
       break;
@@ -51,19 +47,19 @@ int bisection(double xl, double xu, double Delta, fptr f)
     else {
       xl = xr;
     }
-    N++;
+ 
   }
 
   return N;
 }
 
-//Para Regulafalsi y Biseccion, N inicia con 1
 
 int regulafalsi(double xl, double xu, double Delta, fptr f)
 {
   double xr = xl;
-  int N = 1;
+  int N = 0;
   while(1) {
+    N++;
     xr = xu - (f(xu)*(xl-xu))/(f(xl) -f(xu));
     if (std::fabs(f(xr)) <= Delta) {
       break;
@@ -74,26 +70,14 @@ int regulafalsi(double xl, double xu, double Delta, fptr f)
     else {
       xl = xr;
     }
-    N++;
+    
   }
 
 
   return N;
 }
 
-/*double fixedpoint(double x0, double Delta, fptr f, fptr g)
-{
-  double xr = x0;
-  int N = 1;
-  while(1) {
-    if (std::fabs(f(xr)) <= Delta) {
-      break;
-    }
-    xr = g(xr);
-    N++;
-  }
-  return N;
-  }*/
+
 
 double fun(double x)
 {
@@ -119,7 +103,6 @@ int newton_raphson(double x0, double Delta, fptr f, fptr fderiv)
   return N;
 }
 
-//Para Newton Inicia con N=0
 
 double deriv(double x)
 {
